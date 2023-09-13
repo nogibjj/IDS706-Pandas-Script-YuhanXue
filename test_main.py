0e3ea696b75b61ef19daf31425ad26e3c60d2d94
Yuhan <yx167@duke.edu>
2023-09-13 00:33:25 -0400

    pass test

--- test_main.py
+++ test_main.py
@@ -1,10 +1,11 @@
 from main import *
 
+
 def test():
-  data = pd.read_csv('diabetes.csv')
-  assert(data['Age'].mean() == get_mean(data, 'Age'))
+    data = pd.read_csv("diabetes.csv")
+    assert data["Age"].mean() == get_mean(data, "Age")
 
 
-if __name__ == '__main__':
-  test()
-  print('CICD Passed.')
+if __name__ == "__main__":
+    test()
+    print("CICD Passed.")
